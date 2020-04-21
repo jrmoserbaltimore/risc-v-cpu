61311391612a745a3c56bde41f60afab76f51190
John Moser <john.r.moser@gmail.com>
2020-04-21 07:56:54 -0400

    Improve .gitignore

---
+++ RISC-V.srcs/simulation_tests/barrel_shifter_test.vhdl
@@ -0,0 +1,31 @@
+-- vim: set ts=4 sw=4 et
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+use IEEE.math_real."ceil";
+use ieee.math_real."log2";
+use work.e_barrel_shifter;
+
+entity et_barrel_shift is
+    generic (
+        XLEN      : natural
+    );
+    port(
+        Dout       : out std_ulogic_vector(XLEN-1 downto 0)
+    ); 
+end et_barrel_shift;
+
+architecture t_barrel_shift of et_barrel_shift is
+begin
+    barrel_shifter: entity e_barrel_shifter(barrel_shifter)
+    generic map (
+        XLEN      => 8
+    )
+    port map (
+        Din        => "10110101",  -- output should be 10101000
+        Shift      => "0011", -- 3
+        opFlags    => ('0', '0', '0', '0'), -- rsh, ar
+        Dout       => DOut
+    );
+end t_barrel_shift;
